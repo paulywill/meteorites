5e559038c197a457b9a7c64d11f483f2a555665f
Paul Gamble <pgamble@gmail.com>
2019-05-18 01:06:40 -0400

    ✨  - add simple search bar; empty components dir

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Meteorite Explorer</h1>
+
+        <form>
+          <label>
+            Search:
+          <input type="text" name="searchTerm" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+
+        <p>https://data.nasa.gov/resource/gh4g-9sfh.json</p>
       </header>
     </div>
   );
