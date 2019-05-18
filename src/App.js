45e932cb5a4871936b24fd640daaf7c0fbf2f837
Paul Gamble <pgamble@gmail.com>
2019-05-18 01:31:08 -0400

    🎨  - created Dashboard componet; added axios dep

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from 'react';
+import Dashboard from './components/Dashboard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Meteorite Explorer</h1>
-
-        <form>
-          <label>
-            Search:
-          <input type="text" name="searchTerm" />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-
-        <p>https://data.nasa.gov/resource/gh4g-9sfh.json</p>
-      </header>
+      <Dashboard />
     </div>
   );
 }
