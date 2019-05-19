38d9797eecad0632260cedb7612d46edfc0a9f98
Paul Gamble <pgamble@gmail.com>
2019-05-18 23:59:29 -0400

    👽  - able to download full dataset and 43K rows
    There some more information about the SODA API [here](https://support.socrata.com/hc/en-us/articles/202949268-How-to-query-more-than-1000-rows-of-a-dataset)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import Dashboard from './components/Dashboard';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
+const API_URL = 'https://data.nasa.gov/resource/gh4g-9sfh.json?$limit=500000';
 
-function App() {
-  return (
-    <div className="App">
-      <Dashboard />
-    </div>
-  );
+class App extends Component {
+  state = {
+    meteorite: []
+  }
+  componentDidMount() {
+    const url = `${API_URL}`;
+    axios.get(url).then(response => response.data)
+      .then((data) => {
+        this.setState({ meteorite: data })
+        console.log(this.state.meteorite)
+      })
+  }
+  
+  
+  
+  
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <h1>Meteorite Explorer</h1>
+
+          <form>
+            <label>
+              Search: <input type="text" name="name" />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+
+        </header>
+      </div >
+    );
+  }
 }
-
 export default App;
